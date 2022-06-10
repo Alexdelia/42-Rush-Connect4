be16f3996a27a0265a8a94d8ac6ed32093afd161
Alexdelia <alexandre.delille.57@gmail.com>
2022-06-10 23:36:31 +0200

    ai doesn't work

--- src/ai.c
+++ src/ai.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 00:04:04 by adelille          #+#    #+#             */
-/*   Updated: 2022/06/10 22:15:43 by adelille         ###   ########.fr       */
+/*   Updated: 2022/06/10 23:35:02 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ static bool	is_winning_move(t_board b, const t_index index)
 	return (is_connect(&b, b.n_move % 2 + 1));
 }
 
-static int	negamax(t_board b, const t_index next_move)
+static int	negamax(t_board b, const t_index next_move, int alpha, int beta)
 {
 	t_index	index;
-	int		best_score;
-	int		tmp;
+	int		score;
+	int		max;
 
 	add_coin(&b, next_move, b.n_move % 2 + 1);
 	if (is_board_full(&b))
@@ -31,44 +31,57 @@ static int	negamax(t_board b, const t_index next_move)
 	while (++index < b.col)
 		if (!is_col_full(&b, index) && is_winning_move(b, index))
 			return ((b.col * b.row + 1 - b.n_move) / 2);
-	best_score = -b.col * b.row;
+	max = (b.col * b.row - 1 - b.n_move) / 2;
+	if (beta > max)
+	{
+		beta = max;
+		if (alpha >= beta)
+			return (beta);
+	}
 	index = -1;
 	while (++index < b.col)
 	{
 		if (!is_col_full(&b, index))
 		{
-			tmp = -negamax(b, index);
-			if (tmp > best_score)
-				best_score = tmp;
+			score = -negamax(b, index, -beta, -alpha);
+			if (score >= beta)
+				return (score);
+			if (score > alpha)
+				alpha = score;
 		}
 	}
-	return (best_score);
+	return (alpha);
 }
 
-static t_index	solve(t_board b)
+static t_index	solve(t_board b, int alpha, int beta)
 {
 	t_index	index;
 	t_index	best_index;
-	int		best_score;
-	int		tmp;
+	int		score;
+	int		max;
 
 	index = -1;
 	while (++index < b.col)
 		if (!is_col_full(&b, index) && is_winning_move(b, index))
 			return (index);
-	best_score = -b.col * b.row;
+	best_index = 0;
+	max = (b.col * b.row - 1 - b.n_move) / 2;
+	if (beta > max)
+		beta = max;
 	index = -1;
 	while (++index < b.col)
 	{
 		if (!is_col_full(&b, index))
 		{
-			tmp = -negamax(b, index);
-			if (tmp > best_score)
+			score = -negamax(b, index, -beta, -alpha);
+			if (score >= beta)
+				return (best_index);
+			if (score > alpha)
 			{
-				best_score = tmp;
+				alpha = score;
 				best_index = index;
 				ft_ps("\r");
-				ft_pn(best_score);
+				ft_pn(alpha);
 				ft_ps("  ");
 			}
 		}
@@ -78,7 +91,9 @@ static t_index	solve(t_board b)
 
 void	ai(t_env *e)
 {
-	if (!add_coin(&e->b, solve(e->b), AI))
+	//	if (!add_coin(&e->b, solve(e->b, -1, 1), AI))	// weak
+	if (!add_coin(&e->b, solve(e->b, -e->b.col * e->b.row / 2,
+				e->b.col * e->b.row / 2), AI))
 	{
 		ft_pser(C_BOLD);
 		ft_pserc("AI tried to insert coin on full col\n", RED);
